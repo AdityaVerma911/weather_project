77fca62af91177d5e2e8aba774db38c439ee6477
adityver <aditya.verma@antiersolutions.com>
2024-11-24 19:29:12 +0530

    final commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+  def country_codes
+    [
+        ['United States', 'US'],
+        ['United Kingdom', 'GB'],
+        ['India', 'IN'],
+        ['Netherlands', 'NL'],
+        ['Germany', 'DE'],
+        ['France', 'FR'],
+        ['Australia', 'AU'],
+        ['Canada', 'CA'],
+        ['Japan', 'JP'],
+        ['China', 'CN']
+    ]
+    end
+end
